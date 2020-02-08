aff7197384197667fd5fe40d359e083f1058b432
youngconnor <you@youremail.com>
2020-02-07 22:27:24 -0600

    First Commit

---
+++ R/My First Lab Function.R
@@ -0,0 +1,16 @@
+#' Myplot Functions
+#'
+#'This Function Calculates the quadratic curve for the spruce df set of data
+#'
+#'Has the coefficients to create the plot and equation for the quadratic equation in the spruce data
+#'
+#' @param x a vector of double values
+#'
+#' @return a vector of doubles
+#' @export
+#'
+#' @examples
+#' x:2; myplot(x)
+myplot=function(x){
+  0.86089580 + 1.46959217*x - 0.02745726*x^2
+}
