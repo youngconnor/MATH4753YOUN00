c760355805900cb2e2997e42c3d2917f62423635
youngconnor <you@youremail.com>
2020-03-05 17:27:58 -0600

    added fire data to package

--- R/Lab 6 function.R
+++ R/Lab 6 function.R
@@ -3,19 +3,23 @@
 #' @param mu
 #' @param sigma
 #'
-#' @return
+#' @return a list of vectors
 #' @export
 #'
-#' @examples
-myncurve = function(mu, sigma){
-  curve(dnorm(a,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
-  xcurve=seq(-Inf,a,length=1000)
+#' @examples x:1:20
+#'
+#'
+#'
+myncurve = function(mu, sigma, a){
+  plot.new
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+  xcurve=seq(-30,a,length=1000)
   ycurve=dnorm(xcurve,mean=mu,sd=sigma)
-  polygon(c(,xcurve,25),c(0,ycurve,0),col="Red")
+  polygon(c(-30,xcurve,a),c(0,ycurve,0),col="Red")
   prob=pnorm(a,mean=mu,sd=sigma)-pnorm(-Inf,mean=mu,sd=sigma)
   prob=round(prob,4)
   text(x=2, y=0.1, paste("Area = ", prob, sep=""))
 
 }
 
-myncurve(10, 5, 7)
+
--- R/fire.R
+++ R/fire.R
@@ -1,1 +1,15 @@
+#' Fire data set.
+#'
+#' fire data
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance in cm}
+#'   \item{DAMAGE}{damage ammount}
+#'   ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
 fire=read.csv("FIREDAM.csv")
+saveRDS(fire, file="fire")
+usethis::use_data(fire)
