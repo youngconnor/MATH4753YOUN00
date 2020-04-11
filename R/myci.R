a408bdbc934c6b0ccd82f089e95f76f6d7dd7d68
youngconnor <you@youremail.com>
2020-04-10 19:12:12 -0500

    added lab 11 function

---
+++ R/myci.R
@@ -0,0 +1,14 @@
+#' myci
+#'
+#' @param x
+#'
+#' @return a list of vectors containing a 95% confidence interval for mu from a single sample, x
+#' @export
+#'
+#' @examples x= rnorm(30, meam=12, sd=12)
+#'
+#' This function takes a single sample, x, and creates a 95% confidence interval for mu in this single sample.
+#'
+myci=function(x){
+  t.test(x, conf.level = 0.95)
+}
